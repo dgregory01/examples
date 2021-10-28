9004851123b34681f286f995e27f16c810291052
Daniel Gregory <daniel.gregory3@outlook.com>
2021-10-28 17:13:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Examples of Python Code
+
+A place for me to store snippets of Python code and experiment.
---
+++ properties.py
@@ -0,0 +1,6 @@
+class Human:
+
+    def __init__(self, hair_colour, eye_colour):
+        self.hair_colour = hair_colour
+        self.eye_colour = eye_colour
+        
